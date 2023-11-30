39261da33b2199938d65754b0b5fff81b1cebfed
Benoit <fbenoit2077@gmail.com>
2023-11-29 20:01:14 -0500

    javascipt html string added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs=require("fs");
+const fs = require('fs');
 
 inquirer
   .prompt([
@@ -28,4 +28,42 @@ inquirer
       message: 'What is your GitHub URL?',
       name: 'GitHub',
     },
-  ]);
+  ])
+  .then((answers) => {
+    // Function to generate HTML string
+    const generateHTML = ({ name, location, bio, linkedIn, GitHub }) => {
+      return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${name}'s Portfolio</title>
+          <style>
+            body { font-family: Arial, sans-serif; }
+            /* Add more styles here */
+          </style>
+        </head>
+        <body>
+          <h1>${name}</h1>
+          <p>Location: ${location}</p>
+          <p>Bio: ${bio}</p>
+          <p>LinkedIn: <a href="${linkedIn}">${linkedIn}</a></p>
+          <p>GitHub: <a href="${GitHub}">${GitHub}</a></p>
+        </body>
+        </html>
+      `;
+    };
+
+    // Write the HTML string to a file
+    fs.writeFile('portfolio.html', generateHTML(answers), (err) => {
+      if (err) {
+        console.error('An error occurred:', err);
+        return;
+      }
+      console.log('Portfolio created!');
+    });
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
