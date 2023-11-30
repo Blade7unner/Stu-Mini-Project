462b78f253004d4ad656cdf21b02520e1f5af653
Benoit <fbenoit2077@gmail.com>
2023-11-29 20:36:24 -0500

    added css styling to the HTML string

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ inquirer
     },
   ])
   .then((answers) => {
-    // Function to generate HTML string
     const generateHTML = ({ name, location, bio, linkedIn, GitHub }) => {
       return `
         <!DOCTYPE html>
@@ -40,22 +39,47 @@ inquirer
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>${name}'s Portfolio</title>
           <style>
-            body { font-family: Arial, sans-serif; }
-            /* Add more styles here */
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+              color: #333333;
+              margin: 0;
+              padding: 0;
+            }
+            .container {
+              width: 80%;
+              margin: auto;
+              padding: 20px;
+            }
+            h1 {
+              color: #4A90E2;
+            }
+            p {
+              font-size: 1.1em;
+              line-height: 1.5;
+            }
+            a {
+              color: #4A90E2;
+              text-decoration: none;
+            }
+            a:hover {
+              text-decoration: underline;
+            }
           </style>
         </head>
         <body>
-          <h1>${name}</h1>
-          <p>Location: ${location}</p>
-          <p>Bio: ${bio}</p>
-          <p>LinkedIn: <a href="${linkedIn}">${linkedIn}</a></p>
-          <p>GitHub: <a href="${GitHub}">${GitHub}</a></p>
+          <div class="container">
+            <h1>${name}</h1>
+            <p>Location: ${location}</p>
+            <p>Bio: ${bio}</p>
+            <p>LinkedIn: <a href="${linkedIn}" target="_blank">${linkedIn}</a></p>
+            <p>GitHub: <a href="${GitHub}" target="_blank">${GitHub}</a></p>
+          </div>
         </body>
         </html>
       `;
     };
 
-    // Write the HTML string to a file
     fs.writeFile('portfolio.html', generateHTML(answers), (err) => {
       if (err) {
         console.error('An error occurred:', err);
@@ -67,3 +91,4 @@ inquirer
   .catch((error) => {
     console.error('An error occurred:', error);
   });
+
